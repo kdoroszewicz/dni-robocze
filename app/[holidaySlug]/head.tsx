bd28e15febe87712bbed0a961f41f0ae0ac71b4e
Kamil <l3szcz@gmail.com>
2023-03-29 17:45:08 +0200

    add current year to various elements

--- app/[holidaySlug]/head.tsx
+++ app/[holidaySlug]/head.tsx
@@ -2,18 +2,19 @@ import { getHoliday, getHolidaySlug } from "../../services/utils";
 
 export default async function Head({ params }) {
   const holiday = await getHoliday(params);
+  const currentYear = new Date().getFullYear();
 
   if (!holiday) {
     return (
       <>
-        <title>Kalkulator Dni Roboczych</title>
+        <title>Kalkulator Dni Roboczych {currentYear}</title>
       </>
     );
   }
 
   return (
     <>
-      <title>{`${holiday.name} — Kalkulator Dni Roboczych`}</title>
+      <title>{`${holiday.name} — Kalkulator Dni Roboczych ${currentYear}`}</title>
       <link
         rel="canonical"
         href={`https://kalkulatordniroboczych.pl/${getHolidaySlug(

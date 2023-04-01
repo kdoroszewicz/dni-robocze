55cc12aaea07bf1470358b2a811f68d31f5a912c
Kamil <l3szcz@gmail.com>
2023-04-01 16:01:12 +0200

    add ga

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 // import { Providers } from "./providers";
 import BaseLayout from "../components/BaseLayout";
 import "../styles/global.css";
+import GoogleAnalytics from "./GoogleAnalytics";
 
 export default function RootLayout({
   // Layouts must accept a children prop.
@@ -11,6 +12,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="pl">
+      <GoogleAnalytics GA_TRACKING_ID={process.env.GA_TRACKING_ID} />
       <body className="bg-gray-50">
         {/* <Providers> */}
         <BaseLayout>{children}</BaseLayout>

591031bb47dd2b1eedb3b86aeb800498272b27b7
Kamil <l3szcz@gmail.com>
2023-05-28 10:48:13 +0200

    remove it only

--- src/__tests__/workDaysUtils.js
+++ src/__tests__/workDaysUtils.js
@@ -63,7 +63,7 @@ describe("getWorkDays", () => {
     expect(result).toBe(2);
   });
 
-  it.only("should return 21 work days between 2023-05-01 and 2023-05-31", () => {
+  it("should return 21 work days between 2023-05-01 and 2023-05-31", () => {
     const earlierDate = new Date("2023-05-01");
     const laterDate = new Date("2023-05-31");
     const result = getWorkDays(laterDate, earlierDate);

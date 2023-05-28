98afd8ef76fe006a22b1027f24ae90becce3a632
Kamil <l3szcz@gmail.com>
2023-05-28 16:26:34 +0200

    add Inter font

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,15 +2,23 @@ import BaseLayout from "../components/BaseLayout";
 import "./globals.css";
 import GoogleAnalytics from "../components/GoogleAnalytics";
 
+import { Inter } from "next/font/google";
+
+// If loading a variable font, you don't need to specify the font weight
+const inter = Inter({
+  subsets: ["latin"],
+  display: "swap",
+});
+
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
   return (
-    <html lang="pl">
+    <html lang="pl" className={inter.className}>
       <GoogleAnalytics GA_TRACKING_ID={process.env.GA_TRACKING_ID!} />
-      <body className="bg-gray-50">
+      <body>
         {/* <Providers> */}
         <BaseLayout>{children}</BaseLayout>
         {/* </Providers> */}

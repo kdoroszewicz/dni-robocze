cd72e6e09cdf08ef8d8fbf7381599f4622bf5c78
Kamil Doroszewicz <7294362+kdoroszewicz@users.noreply.github.com>
2023-06-09 22:37:21 +0200

    Revert "Add ads"

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,6 @@ import "./globals.css";
 import GoogleAnalytics from "../components/GoogleAnalytics";
 
 import { Inter } from "next/font/google";
-import Adsense from "@/components/Adsense";
 
 // If loading a variable font, you don't need to specify the font weight
 const inter = Inter({
@@ -19,9 +18,6 @@ export default function RootLayout({
   return (
     <html lang="pl" className={inter.className}>
       <GoogleAnalytics GA_TRACKING_ID={process.env.GA_TRACKING_ID!} />
-      <Adsense
-        NEXT_PUBLIC_GOOGLE_ADSENSE={process.env.NEXT_PUBLIC_GOOGLE_ADSENSE!}
-      />
       <body>
         {/* <Providers> */}
         <BaseLayout>{children}</BaseLayout>

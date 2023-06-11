a04966f597f1ef138d44cfa426cd32c7fb653774
Kamil <l3szcz@gmail.com>
2023-06-11 10:10:50 +0200

    fix overflow

--- src/components/BaseLayout.tsx
+++ src/components/BaseLayout.tsx
@@ -6,7 +6,7 @@ type LayoutProps = {
 
 const Layout = ({ children }: LayoutProps) => {
   return (
-    <div className="layout-wrapper relative flex justify-center">
+    <div className="layout-wrapper relative flex justify-center overflow-hidden">
       <div className="layout relative mt-[44px] h-full w-full max-w-[782px] px-4 md:mx-0 md:mt-16">
         <div>{children}</div>
       </div>

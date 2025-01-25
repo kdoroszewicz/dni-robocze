60457d3a12152f23b6b0451fafea180d33b04cf0
Kamil <l3szcz@gmail.com>
2025-01-25 15:36:53 +0100

    Migrate Next.js configuration to ESM and update dependencies

---
+++ next.config.mjs
@@ -0,0 +1,25 @@
+import createMDX from '@next/mdx'
+/**
+ * Vercel runs builds in UTC so force it locally
+ * to avoid server-client mismatches
+ */
+process.env.TZ = "UTC";
+console.log(
+  `Running with timezone ${Intl.DateTimeFormat().resolvedOptions().timeZone}`
+);
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  // Configure `pageExtensions` to include markdown and MDX files
+  pageExtensions: ['js', 'jsx', 'md', 'mdx', 'ts', 'tsx'],
+  // Optionally, add any other Next.js config below
+  transpilePackages: ['react-day-picker']
+
+}
+ 
+const withMDX = createMDX({
+  // Add markdown plugins here, as desired
+})
+ 
+// Merge MDX config with Next.js config
+export default withMDX(nextConfig)

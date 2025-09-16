46c11091e6cec1c1000f0aa4e35488ea2fe2f9fc
Kamil <l3szcz@gmail.com>
2025-09-16 20:11:47 +0200

    fix spacing for headings

--- src/components/ui/typography.tsx
+++ src/components/ui/typography.tsx
@@ -23,7 +23,7 @@ export function H2({
   return (
     <h2
       className={cn(
-        "scroll-m-20 border-b pb-2 text-3xl font-semibold tracking-tight first:mt-0",
+        "mt-8 scroll-m-20 border-b pb-2 text-3xl font-semibold tracking-tight first:mt-0",
         className
       )}
       {...props}

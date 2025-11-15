38bd3d18ca0aaa4b95bedad1a5d27f6d8c5d78fe
Kamil <l3szcz@gmail.com>
2025-11-15 22:44:44 +0100

    add time base revalidation support

--- open-next.config.ts
+++ open-next.config.ts
@@ -1,6 +1,12 @@
 import { defineCloudflareConfig } from "@opennextjs/cloudflare";
 import r2IncrementalCache from "@opennextjs/cloudflare/overrides/incremental-cache/r2-incremental-cache";
+import doQueue from "@opennextjs/cloudflare/overrides/queue/do-queue";
 
 export default defineCloudflareConfig({
+  // R2 incremental cache for ISR data
   incrementalCache: r2IncrementalCache,
+  // Durable Object–backed queue for time-based revalidation
+  // Matches the "small site using revalidation" setup in the docs:
+  // https://opennext.js.org/cloudflare/caching#small-site-using-revalidation
+  queue: doQueue,
 });

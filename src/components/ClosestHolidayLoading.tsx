924fba87306403d4584204b8e3d59fea913b5a7a
Kamil <l3szcz@gmail.com>
2025-11-19 14:09:30 +0100

    cache component for closest holiday

---
+++ src/components/ClosestHolidayLoading.tsx
@@ -0,0 +1,7 @@
+import { Skeleton } from "./ui/skeleton";
+
+export const ClosestHolidayLoading = () => {
+  return (
+    <Skeleton className="h-[30px] w-full max-w-[520px] rounded-[14px] bg-[#E8F6FF]" />
+  );
+};
